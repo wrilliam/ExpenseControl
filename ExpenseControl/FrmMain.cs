c96aff3357b7803c1cd77fe875fa2b550968455f
W. Ribeiro <william.eti@alu.ufc.br>
2021-01-09 18:22:30 -0300

    Managing database - part I

---
+++ ExpenseControl/FrmMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpenseControl
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void billsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.billsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.expensesDataSet);
+
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'expensesDataSet.Bills' table. You can move, or remove it, as needed.
+            this.billsTableAdapter.Fill(this.expensesDataSet.Bills);
+
+        }
+    }
+}
